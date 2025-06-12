4745553110e1ed52117114bda922e93990912c56
Mahdi <imahdimir@gmail.com>
2025-06-12 15:36:51 -0700

    commit before chaning the name of the repo to add g4 for keeping the relation to the CF repo

---
+++ b_000_env.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+
+class Directory :
+    raw_data_2nd_pilot = Path('/Users/mmir/Library/CloudStorage/Dropbox/ReRA/RiskPreferences-OTree Online Survey 2024-Raw Data/2nd Pilot')
+
+
+
+class FilePath :
+    d = Directory()
+
+    all_apps_wide_csv_250301 = d.raw_data_2nd_pilot / 'all_apps_wide_2025-03-01.csv'
+
+
+
+class Constants :
+    mturk_id = 'Introduction.1.player.MTurk_ID'
+
+
+class Parameters :
+    pass
+
+
+
+# temp
+d = Directory()
+fp = FilePath()
+c = Constants()
+
+
+##
+def _draft() :
+    pass
+
+    ##
+
+
+
+    ##
+
+
+
+    ##
+
+
+
+    ##
